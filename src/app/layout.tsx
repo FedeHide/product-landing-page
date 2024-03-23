17cf899424ac7e06977b1b7b323d33c73687baca
Fede Hide <larcdian@gmail.com>
2024-03-23 15:41:27 -0300

    Update layout.tsx
    update OG image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
 		title: 'Handcraft Guitars',
 		description: 'This is a fake product landing page, a freecodecamp assignment',
 		images: [
-			'https://ogcdn.net/6064b869-74ed-4eb9-b76c-0b701ffe7e6b/v4/fedehide.github.io//https%3A%2F%2Fopengraph.b-cdn.net%2Fproduction%2Fdocuments%2F0f41fa24-57dd-406d-8308-663ac2723fec.png%3Ftoken%3DlLIyHdZloHXqDkYB3CYPNXqfPZ_4Ei9s6oDauBOqnec%26height%3D558%26width%3D1200%26expires%3D33241972125/og.png',
+			'https://raw.githubusercontent.com/FedeHide/product-landing-page/main/public/assets/handcraft-guitars.webp',
 		],
 	},
 	twitter: {
